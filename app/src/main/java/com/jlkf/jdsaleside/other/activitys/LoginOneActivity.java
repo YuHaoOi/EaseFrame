4b2a0005a9ed7b36a05fd78df492a569cfad86cf
yu.hao <ddddd>
2018-04-20 15:01:14 +0800

    添加注册

---
+++ app/src/main/java/com/jlkf/jdsaleside/other/activitys/LoginOneActivity.java
@@ -0,0 +1,15 @@
+package com.jlkf.jdsaleside.other.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jlkf.jdsaleside.R;
+
+public class LoginOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_one);
+    }
+}
